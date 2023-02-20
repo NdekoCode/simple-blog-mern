363fba9a1428aae072f89f96f80da322fa60c2be
NdekoCode <arickbulakali@gmail.com>
2023-02-20 13:20:07 +0200

    feat: add main routes like post

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { BASE_API } from "./libs/constants.js";
+import postsRouter from "./routes/posts.routes.js";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+app.use(BASE_API + "/posts", postsRouter);
 export default app;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+dotenv.config();
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => {
   console.info("Server is running at http://localhost:" + PORT);
